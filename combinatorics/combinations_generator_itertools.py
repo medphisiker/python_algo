c99b4ed597df1ad11c2d468491401784dc7fbd1c
Anton Shiryaev <shiryaev_anton@cashalot.co>
2024-07-31 21:10:37 +1000

    style(code formating): auto code formating with Black
    auto code formating with Black

--- combinatorics/combinations_generator_itertools.py
+++ combinatorics/combinations_generator_itertools.py
@@ -1,10 +1,11 @@
 from itertools import combinations
+
 if __name__ == "__main__":
     # генерирует все возможные сочетания объектов из n по k
     n = 5
     k = 3
-    
-    set_of_nums = list(range(1, n+1))
-    
+
+    set_of_nums = list(range(1, n + 1))
+
     for i in combinations(set_of_nums, k):
         print(i)
